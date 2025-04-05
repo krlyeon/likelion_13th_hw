2ede268b86979e434169bea16acf82ec580d6d1e
krlyeon <sooyeon8427@gmail.com>
2025-04-06 02:32:29 +0900

    2주차 장고 과제

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def summarypage(request):
+    return render (request, 'main/summarypage.html')
+
+def intropage(request):
+    return render (request, 'main/intropage.html')
